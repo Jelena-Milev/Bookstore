6be12e014f440d1a47e3af311487b07d4e4a4bdc
Jelena-Milev <jeka.milev@gmail.com>
2020-08-13 02:17:50 +0200

    bookstorage: project initialized

---
+++ bookstorage/src/test/java/com/fon/njt/bookstorage/BookStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fon.njt.bookstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

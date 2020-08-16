3a3d1d53bdf1720571f43161f66e5b57acf10968
Jelena-Milev <jeka.milev@gmail.com>
2020-08-16 02:52:50 +0200

    orderservice: request dto validations

--- orderservice/src/main/java/com/fon/njt/orderservice/dto/request/OrderItemRequestDto.java
+++ orderservice/src/main/java/com/fon/njt/orderservice/dto/request/OrderItemRequestDto.java
@@ -2,11 +2,18 @@ package com.fon.njt.orderservice.dto.request;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
 public class OrderItemRequestDto {
+    @NotNull(message = "Book id must not be null")
+    @Positive(message = "Book id must be valid id")
     private Long bookId;
+    @NotNull(message = "Number of book pieces must not be null")
+    @Positive(message = "Number of book pieces must be larger than zero")
     private Integer quantity;
 }
--- orderservice/src/main/java/com/fon/njt/orderservice/dto/request/OrderRequestDto.java
+++ orderservice/src/main/java/com/fon/njt/orderservice/dto/request/OrderRequestDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @NoArgsConstructor
@@ -12,6 +16,10 @@ import java.util.List;
 @Getter
 @Builder
 public class OrderRequestDto {
+    @NotNull(message = "User id must not be null")
+    @Positive(message = "User id must be valid id")
     private Long userId;
-    private List<OrderItemRequestDto> items;
+    @NotNull(message = "Order items must not be null")
+    @NotEmpty(message = "Order items must not be empty")
+    private List<@Valid OrderItemRequestDto> items;
 }

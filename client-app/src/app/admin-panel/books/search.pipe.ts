1529daeb0ef15682e1fb54d2356ec2e7b33e486e
Jelena-Milev <jeka.milev@gmail.com>
2020-08-26 18:22:18 +0200

    client-app:pipes for books search improvement
    books and publishers filtered based on matching with start of each word in book title or publisher name

--- client-app/src/app/admin-panel/books/search.pipe.ts
+++ client-app/src/app/admin-panel/books/search.pipe.ts
@@ -1,22 +1,22 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Book } from './book.model';
+import { Pipe, PipeTransform } from "@angular/core";
+import { Book } from "./book.model";
 
 @Pipe({
-  name: 'searchBooks'
+  name: "searchBooks",
 })
 export class SearchPipe implements PipeTransform {
-
   transform(items: Book[], searchText: string): Book[] {
     if (!items) {
       return [];
     }
-    if (!searchText || searchText === '') {
+    if (!searchText || searchText === "") {
       return items;
     }
     searchText = searchText.toLocaleLowerCase();
 
-    return items.filter(it => {
-      return it.title.toLocaleLowerCase().indexOf(searchText) !== -1;
+    return items.filter((it) => {
+      const titleWords = it.title.toLocaleLowerCase().split(" ");
+      return titleWords.findIndex((word) => word.startsWith(searchText)) !== -1;
     });
   }
 }
--- client-app/src/app/admin-panel/publishers/search.pipe.ts
+++ client-app/src/app/admin-panel/publishers/search.pipe.ts
@@ -1,23 +1,22 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Publisher } from './publisher.model';
+import { Pipe, PipeTransform } from "@angular/core";
+import { Publisher } from "./publisher.model";
 
 @Pipe({
-  name: 'searchPublishers'
+  name: "searchPublishers",
 })
 export class SearchPipe implements PipeTransform {
-
   transform(items: Publisher[], searchText: string): Publisher[] {
     if (!items) {
       return [];
     }
-    if (!searchText || searchText === '') {
+    if (!searchText || searchText === "") {
       return items;
     }
     searchText = searchText.toLocaleLowerCase();
 
-    return items.filter(it => {
-      return it.name.toLocaleLowerCase().startsWith(searchText);
+    return items.filter((it) => {
+      const publNameWords = it.name.toLocaleLowerCase().split(" ");
+      return ( publNameWords.findIndex((word) => word.startsWith(searchText)) !== -1 );
     });
   }
-
 }
